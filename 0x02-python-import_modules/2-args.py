d365aafa9f8e5cf9993cd35f18f2e1a02534f0a1
Jenny2291 <asikadijenny@gmail.com>
2023-10-11 23:55:56 +0100

    updated

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+if __name__== " __main__":
+    import sys
+    i = len(sys.arg) -1
+
+    if 1 == 0:
+        print("{} arguments:".format(i))
+    elif i == 1:
+        print("{} argument:".format(i))
+    else:
+        print("{} argument:".format(i))
+
+        if i >= i:
+            i = 0
+            for arg in sys.arg:
+                if i != 0:
+                    print("{}: {}".format(i, arg))
+                    i += 1
+                    
